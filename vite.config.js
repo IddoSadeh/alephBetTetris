2d40d1c3b30e22988c9c01ae435f80c802e27daa
IddoSadeh <iddosadeh@gmail.com>
2024-06-18 23:33:27 -0700

    test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,24 @@
-// vite.config.js
-export default {
-    root: '.',
-    build: {
-      outDir: 'dist'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+export default defineConfig({
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'fonts',
+          dest: '.'
+        }
+      ]
+    })
+  ],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      }
     }
-  };
-  
+  }
+});
