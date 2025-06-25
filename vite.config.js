05c15ac78f6dabf9aded4ab3f349dc85f1004c82
IddoSadeh <iddosadeh@gmail.com>
2025-06-25 12:19:14 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,21 @@ import { resolve } from 'path';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
+  base: '/ALEPHBETTETRIS/', // Replace with your GitHub repo name
   plugins: [
     viteStaticCopy({
       targets: [
-        { src: 'fonts', dest: '.' },
-        { src: 'images', dest: '.' },
-        { src: 'icons', dest: '.' }
+        { src: 'fonts/**/*', dest: 'fonts' },
+        { src: 'images/**/*', dest: 'images' },
+        { src: 'icon/**/*', dest: 'icon' },
+        { src: '98.css', dest: '.' },
+        { src: 'styles.css', dest: '.' }
       ]
     })
   ],
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
